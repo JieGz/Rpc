6bd2c87524e701ff0922242c9e413eb6e880c920
Luke <gdjiegz@gmail.com>
2020-11-22 17:02:34 +0800

    feat: 通过Arrays#toString打印一个组件,这可能是一个调试的技巧

--- src/main/java/com/qiyue/App.java
+++ src/main/java/com/qiyue/App.java
@@ -1,9 +1,15 @@
 package com.qiyue;
 
+import java.util.Arrays;
+
 public class App {
 
 	public static void main(String[] args) {
 		//在 VM options 增加-Dluke=111,也可以用过java -jar -Dluke=111 app.jar的方式
 		System.out.println(System.getProperty("luke"));
+
+
+		String[] arr = new String[]{"a", "b", "c"};
+		System.out.println(Arrays.toString(arr));
 	}
 }
